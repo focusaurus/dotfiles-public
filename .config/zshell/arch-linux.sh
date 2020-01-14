b38e5148f55a6841790463c499da468bd88f27a1
Peter Lyons <pete@peterlyons.com>
2020-01-14 07:18:23 -0700

    Simplify save-arch-packages

--- .config/zshell/arch-linux.sh
+++ .config/zshell/arch-linux.sh
@@ -8,20 +8,12 @@ search-arch-packages() {
 }
 # TODO curl -s "https://get.sdkman.io" | bash
 
-save-installed-packages() {
-  (pacman -Qet | awk '{print $1}' | {
-    while IFS= read -r name; do
-      if pacman -Ss "${name}" >/dev/null; then
-        echo "pacman:${name}"
-      else
-        echo "yay:${name}"
-      fi
-    done
-  }) | sort | tee >"${HOME}/.config/arch-linux/pacman-qet-$(uname -n).txt"
+save-arch-packages() {
+  pacman -Qet | awk '{print $1}' | sort | tee >"${HOME}/.config/arch-linux/pacman-qet-$(uname -n).txt"
 }
 
-uninstall-via-pacman() {
-  sudo pacman -R "$@"
+uninstall-arch-package() {
+  sudo yay -R "$@"
 }
 
 sample-rofi-themes() {
