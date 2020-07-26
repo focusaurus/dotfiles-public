036011e26f2b60de9a41a62c3ef9981cfebe60ad
Peter Lyons <pete.lyons@mailchimp.com>
2020-07-25 19:52:05 -0600

    Extract signals.lua
    rc.lua fits on one screen now!

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -26,93 +26,24 @@ terminal = "termite"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
-
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-  awful.layout.suit.floating, awful.layout.suit.tile,
+  awful.layout.suit.max, awful.layout.suit.floating
+  -- awful.layout.suit.tile,
   -- awful.layout.suit.tile.left,
   -- awful.layout.suit.tile.bottom,
   -- awful.layout.suit.tile.top,
-  awful.layout.suit.fair, -- awful.layout.suit.fair.horizontal,
+  -- awful.layout.suit.fair, -- awful.layout.suit.fair.horizontal,
   -- awful.layout.suit.spiral,
   -- awful.layout.suit.spiral.dwindle,
-  awful.layout.suit.max, -- awful.layout.suit.max.fullscreen,
   -- awful.layout.suit.magnifier,
-  awful.layout.suit.corner.nw
+  -- awful.layout.suit.corner.nw
   -- awful.layout.suit.corner.ne,
   -- awful.layout.suit.corner.sw,
   -- awful.layout.suit.corner.se,
 }
--- }}}
 require("keys")
 require("focus")
 require("wibar")
 require("rules")
-
--- {{{ Signals
--- Signal function to execute when a new client appears.
-client.connect_signal("manage", function(c)
-  -- Set the windows at the slave,
-  -- i.e. put it at the end of others instead of setting it master.
-  -- if not awesome.startup then awful.client.setslave(c) end
-
-  if awesome.startup and not c.size_hints.user_position and
-      not c.size_hints.program_position then
-    -- Prevent clients from being unreachable after screen count changes.
-    awful.placement.no_offscreen(c)
-  end
-end)
-
--- Add a titlebar if titlebars_enabled is set to true in the rules.
-client.connect_signal("request::titlebars", function(c)
-  -- buttons for the titlebar
-  local buttons = gears.table.join(awful.button({}, 1, function()
-    c:emit_signal("request::activate", "titlebar", {raise = true})
-    awful.mouse.client.move(c)
-  end), awful.button({}, 3, function()
-    c:emit_signal("request::activate", "titlebar", {raise = true})
-    awful.mouse.client.resize(c)
-  end))
-
-  awful.titlebar(c, {size = 30}):setup{
-    { -- Left
-      awful.titlebar.widget.iconwidget(c),
-      buttons = buttons,
-      layout = wibox.layout.fixed.horizontal
-    },
-    { -- Middle
-      { -- Title
-        align = "center",
-        widget = awful.titlebar.widget.titlewidget(c)
-      },
-      buttons = buttons,
-      layout = wibox.layout.flex.horizontal
-    },
-    { -- Right
-      awful.titlebar.widget.floatingbutton(c),
-      awful.titlebar.widget.maximizedbutton(c),
-      awful.titlebar.widget.stickybutton(c),
-      awful.titlebar.widget.ontopbutton(c),
-      awful.titlebar.widget.closebutton(c),
-      layout = wibox.layout.fixed.horizontal()
-    },
-    layout = wibox.layout.align.horizontal
-  }
-end)
-
--- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-  c:emit_signal("request::activate", "mouse_enter", {raise = false})
-end)
-
-client.connect_signal("focus",
-                      function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("unfocus",
-                      function(c) c.border_color = beautiful.border_normal end)
--- }}}
+require("signals")
