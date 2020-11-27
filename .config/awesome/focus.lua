0fabf6fb70337c63e86bd0fad4115a462dd28dfb
Peter Lyons <pete.lyons@mailchimp.com>
2020-11-27 14:19:15 -0700

    swap hands for app nav and wm nav

--- .config/awesome/focus.lua
+++ .config/awesome/focus.lua
@@ -1,10 +1,24 @@
-local focus = {}
+local module = {}
 
 local awful = require("awful")
-function focus.left() awful.client.focus.byidx(-1) end
-function focus.right() awful.client.focus.byidx(1) end
+
+function module.left()
+  awful.client.focus.byidx(-1)
+end
+
+function module.right()
+  awful.client.focus.byidx(1)
+end
+
+function module.previous()
+  awful.client.focus.history.previous()
+  if client.focus then
+    client.focus:raise()
+  end
+end
+
 -- Enable sloppy focus, so that focus follows mouse.
 -- client.connect_signal("mouse::enter", function(c)
 --     c:activate { context = "mouse_enter", raise = false }
 -- end)
-return focus
+return module
