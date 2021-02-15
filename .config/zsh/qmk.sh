cfd99c307c485b20a5abb04593e8577b2a8d66fe
Peter Lyons <pete.lyons@mailchimp.com>
2021-02-14 20:12:17 -0700

    dotfiles update for freecad et al

--- .config/zsh/qmk.sh
+++ .config/zsh/qmk.sh
@@ -1,4 +1,4 @@
-alias qmkcf="qmk compile && qmk flash"
-alias qmkc="qmk compile"
+alias qmkc="(cd ~/github.com/focusaurus/qmk_firmware && ./util/docker_build.sh ergodox_ez:focusaurus)"
+alias qmkcf="qmkc && qmk flash"
 alias qmkf="qmk flash"
 alias cdqmk="cd ~/github.com/focusaurus/qmk_firmware/ergodox_ez"
