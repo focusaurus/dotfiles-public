b12ceffd13f2be5fc0d5bb7cf2ef172f36a1af27
Peter Lyons <pete@peterlyons.com>
2022-12-03 20:12:23 -0500

    remove stale plugin setup lua modules

--- .config/nvim/lua/plugin-vim-pencil.lua
+++
@@ -1,7 +0,0 @@
--- pencil is a markdown plugin
-vim.api.nvim_create_augroup('pencil', { clear = true })
-vim.api.nvim_create_autocmd('FileType', {
-    group = 'pencil',
-    pattern = { 'markdown', 'md' },
-    command = 'call pencil#init({"wrap":"soft"})',
-})
