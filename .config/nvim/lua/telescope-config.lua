82dd95a2220714b2d030069948d5ee052a54f480
Peter Lyons <pete@peterlyons.com>
2023-01-28 12:02:52 -0500

    Need telescope-config file
    should have added this to git much earlier

---
+++ .config/nvim/lua/telescope-config.lua
@@ -0,0 +1,20 @@
+-- telescope-config.lua
+local module = {}
+
+module.project_files = function()
+  local opts = {} -- define here if you want to define something
+  vim.fn.system('git rev-parse --is-inside-work-tree')
+  if vim.v.shell_error == 0 then
+    require"telescope.builtin".git_files(opts)
+  else
+    require"telescope.builtin".find_files(opts)
+  end
+end
+
+return module
+
+-- call via:
+-- :lua require"telescope-config".project_files()
+
+-- example keymap:
+-- vim.api.nvim_set_keymap("n", "<Leader><Space>", "<CMD>lua require'telescope-config'.project_files()<CR>", {noremap = true, silent = true})
