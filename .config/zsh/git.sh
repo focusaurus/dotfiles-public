d87aca40b1592c94d7cc416383b53f31c71e4667
Peter Lyons <pete@peterlyons.com>
2023-08-07 10:30:49 -0400

    updates

--- .config/zsh/git.sh
+++ .config/zsh/git.sh
@@ -387,7 +387,11 @@ dotfiles-edit-by-search() {
     cd ~ || return 1
     dotfiles-begin
     # shellcheck disable=SC2145
-    git ls-files | grep --invert-match PrusaSlicer | xargs rg -l "$@" | xargs nvim -p -c "/$@"
+    git ls-files |
+      grep --extended-regexp --invert-match '(PrusaSlicer|/Spoons/)' |
+      tr '\n' '\0' |
+      xargs -0 rg -l "$@" |
+      xargs nvim -p -c "/$@"
   )
 }
 
