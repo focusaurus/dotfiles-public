fb7224ce67243bdde80d57b016c3bb1aac9555ce
Peter Lyons <pete@peterlyons.com>
2025-06-26 10:17:41 -0600

    refactor text-editor vs nvim stuff

--- .config/zsh/nvim.sh
+++ .config/zsh/nvim.sh
@@ -1,2 +1,14 @@
 alias nvim-unplugged="nvim -u ~/.config/nvim/unplugged-init.lua -p"
 alias vu=nvim-unplugged
+alias paste-to-vim="te-clipboard"
+alias v="nvim -p"
+#Note to self, use sudoedit to edit files with my nvim config as root
+
+alias nvim-plugin-update="nvim -c PackerSync"
+alias nvim-plugin-install="nvim -c PackerInstall"
+te-vimrc() {
+  (
+    cd ~/.config/nvim
+    ~/bin/text-editor *init.lua lua/*/*.lue lua/*.lua
+  )
+}
