bb138a9cecb1edc7480f2c795f90040a6909592f
Peter Lyons <pete@peterlyons.com>
2025-06-29 11:21:07 -0600

    text-editor fix files-with-matches

--- .config/zsh/text-editor.sh
+++ .config/zsh/text-editor.sh
@@ -1,16 +1,28 @@
-alias te="text-editor"
+alias te="~/bin/text-editor"
 
 te-files-with-matches() {
-  rg --ignore-case --files-with-matches watch "${@}" | xargs ~/bin/text-editor
+  rg --ignore-case --files-with-matches "${@}" | xargs ~/bin/text-editor
 }
 
 te-clipboard() {
-  ~/bin/paste | ~/bin/text-editor -n -c startinsert -
+  # this is admittedly unclean in that in theory
+  # the intention behind ~/bin/text-editor is an integration
+  # point where I could change which program I use as my primary
+  # editor (nvim vs helix vs emacs vs nano or whatever)
+  # and most of my simple scripts would just work once I
+  # adjust ~/bin/text-editor to maintain its external interface
+  #
+  # In this case there are some nvim-specific args here
+  if grep -q -E '^nvim ' ~/bin/text-editor; then
+    ~/bin/paste | ~/bin/text-editor -n -c startinsert -
+  else
+    ~/bin/paste | ~/bin/text-editor
+  fi
 }
 
 te-daily() {
-  org="${HOME}/projects/exocortex/personal"
-  "${EDITOR}" "${org}/$("${org}/bin/file-name-for" 0)"
+  local exo="${HOME}/projects/exocortex/personal"
+  "${EDITOR}" "${exo}/$("${exo}/bin/file-name-for" 0)"
 }
 
 if ~/bin/have-exe nvim; then
