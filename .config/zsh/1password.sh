b18962b78b29d19be41a3ffdadcb53cccfbfcde6
Peter Lyons <pete@peterlyons.com>
2025-10-06 15:24:23 -0600

    1password tweaks

--- .config/zsh/1password.sh
+++ .config/zsh/1password.sh
@@ -1,3 +1,7 @@
+if ~/bin/have-exe op; then
+  eval "$(op completion zsh)"; compdef _op op
+fi
+
 op-add-ssh-key() {
   cat >/dev/null <<'EOF'
 op-add-ssh-key is no longer necessary!
@@ -9,66 +13,3 @@ Just make sure SSH_AUTH_SOCK points to the 1password ssh agent socket.
 EOF
 }
 
-alias oask=op-add-ssh-key
-
-op-copy-password-by-title() {
-  local account="$1"
-  shift
-  session=$(echo -n "\${OP_SESSION_${account}}")
-  # shellcheck disable=SC2154
-  if [[ -z "${session}" ]]; then
-    eval "$(op signin --account "${account}")"
-  fi
-  local items
-  items=$(op item list --format json 2>/dev/null)
-  if [[ -z "${items}" ]]; then
-    eval "$(op signin --account "${account}")"
-    items=$(op item list --format json)
-  fi
-  if [[ -z "${items}" ]]; then
-    return 1
-  fi
-  title=$(echo "${items}" |
-    jq -r ".[].title" |
-    ~/bin/fuzzy-filter "$@")
-  uuid=$(echo "${items}" |
-    jq -r ".[] | select(.title == \"${title}\") | .id")
-  if [[ -z "${uuid}" ]]; then
-    return 1
-  fi
-  echo "${uuid} ${title}"
-  op item get --format json "${uuid}" |
-    jq -r '.fields[] | select(.id=="password").value' |
-    ~/bin/copy
-  echo "Password \"${uuid}${title}\" copied"
-}
-
-op-source-env-by-title() {
-  local account="$1"
-  shift
-  session=$(echo -n "\${OP_SESSION_${account}}")
-  # shellcheck disable=SC2154
-  if [[ -z "${session}" ]]; then
-    eval "$(op signin --account "${account}")"
-  fi
-  local items
-  items=$(op item list --format json 2>/dev/null)
-  if [[ -z "${items}" ]]; then
-    eval "$(op signin --account "${account}")"
-    items=$(op item list --format json)
-  fi
-  if [[ -z "${items}" ]]; then
-    return 1
-  fi
-  title=$(echo "${items}" |
-    jq -r ".[].title" |
-    ~/bin/fuzzy-filter "$@")
-  uuid=$(echo "${items}" |
-    jq -r ".[] | select(.title == \"${title}\") | .id")
-  if [[ -z "${uuid}" ]]; then
-    return 1
-  fi
-  echo "# item found in 1password\n# uuid: ${uuid}\n# title: ${title}"
-  op item get --format json "${uuid}" |
-    jq -r '.fields[] | select(.id=="notesPlain").value'
-}
